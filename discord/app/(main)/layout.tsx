421f6c6e3788e6699827080494d7cd3bcd0503c0
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-05-18 00:36:18 +0700

    add new header

--- discord/app/(main)/layout.tsx
+++ discord/app/(main)/layout.tsx
@@ -14,7 +14,7 @@ const MainLayout = ({
             <MobileHeader/>
             <Sidebar className="hidden lg:flex"/>
             <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-                <div className="bg-red-500 h-full">
+                <div className=" h-full">
 
 
                         {children}
