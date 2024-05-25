6fc5960315acf072cd233469fda71baff9d629ff
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-05-25 18:12:41 +0700

    new course code

---
+++ discord/app/(main)/courses/list.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { courses } from "@/db/schema";
+import { Card } from "./card";
+
+type Props = {
+    coursesList: typeof courses.$inferSelect[];
+    activeCourseId: number;
+}
+
+export const List = ({coursesList,activeCourseId}: Props) => {
+
+    return (
+        <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
+
+            {coursesList.map((course) => (
+                <Card
+                key={course.id}
+                    id={course.id}
+                    title={course.title}
+                    imageSrc={course.imageSrc}
+                    onClick={() => { }}
+                    disabled={false}
+                    active={course.id ===activeCourseId}
+                
+                
+                />
+            ))}
+        </div>
+    )
+}
